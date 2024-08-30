1b035c0121be342767ed6df1cc42fbeb3b27a6df
thanhnt612 <nguyentruongthanh0612@gmail.com>
2024-08-30 08:39:09 +0700

    update

--- routes/webhook.js
+++ routes/webhook.js
@@ -4,6 +4,9 @@ const orderController = require('../controllers/orderController');
 
 const router = express.Router();
 
+router.get("/", (req,res) => {
+  res.json({message:"Welcome to Muha Server"})
+})
 router.post('/webhook-endpoint', webhookController.handleWebhook);
 router.post('/woocommerce_new_order', orderController.handleNewOrder);
 
